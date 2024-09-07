3221c39c92a4a90b9312708b017a04d4b29d6f6c
Vivek Joshi <vrj022@gmail.com>
2024-09-07 10:17:14 +0530

    Logic moved: Job -> Service.

--- app/Jobs/FileScanStatusQueue.php
+++ app/Jobs/FileScanStatusQueue.php
@@ -2,14 +2,11 @@
 
 namespace App\Jobs;
 
-use App\Mail\FileUploadStatus;
+use App\Services\RuleEngineService;
 use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
-use Spatie\SlackAlerts\Facades\SlackAlert;
 
 class FileScanStatusQueue implements ShouldQueue
 {
@@ -17,17 +14,14 @@ class FileScanStatusQueue implements ShouldQueue
 
     public int $tries = 10;
     public int $timeout = 360;
-    private array $data;
-    private array $rules = [
-        'vulnerabilities' => 4,
-    ];
+    public array $fileScanReq;
 
     /**
      * Create a new job instance.
      */
-    public function __construct($data)
+    public function __construct(array $data)
     {
-        $this->data = $data;
+        $this->fileScanReq = $data;
     }
 
     /**
@@ -35,33 +29,8 @@ class FileScanStatusQueue implements ShouldQueue
      */
     public function handle(): void
     {
-        $response = [];
-        $url = env('API_FILE_UPLOAD_STATUS');
-
-        try {
-            $response = Http::withToken($this->data['token'])
-                ->get($url, ['ciUploadId' => $this->data['ciUploadId']]);
-        } catch (Exception $ex) {
-            Log::alert('msg: '. $ex->getMessage());
-        }
-
-        if ($response->successful()) {
-            $mailData['progress'] = $response['progress'];
-
-            if ($response['vulnerabilitiesFound'] > $this->rules['vulnerabilities']) {
-                $mailData['vulnerabilities'] = $response['vulnerabilitiesFound'];
-                SlackAlert::message("Warning: {$response['vulnerabilitiesFound']} vulnerabilities found!");
-            }
-
-            $mailData['detailsUrl'] = $response['detailsUrl'] ?? '';
-
-            Mail::to('vrj022@gmail.com', 'Vivek Joshi')
-                ->send(new FileUploadStatus($mailData));
-
-            if ($response['progress'] < 100) {
-                $this->release(30);
-            }
-        }
+        $progress = RuleEngineService::fileScanStatus($this->fileScanReq);
+        if ($progress >= 0 && $progress < 100) $this->release(30);
     }
 
     public function failed(Exception $ex): void
