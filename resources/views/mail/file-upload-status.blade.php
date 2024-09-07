beabb3bc43e92bf957081d42efb74b98419022d6
Vivek Joshi <vrj022@gmail.com>
2024-09-07 10:14:22 +0530

    CSS File.

--- resources/views/mail/file-upload-status.blade.php
+++ resources/views/mail/file-upload-status.blade.php
@@ -5,22 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>File upload status</title>
-    <style>
-        table {
-            width: 50%;
-            border-collapse: collapse;
-            margin: 25px 0;
-            font-size: 18px;
-            text-align: left;
-        }
-        th, td {
-            padding: 12px 15px;
-            border: 1px solid #ddd;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/mail.css') }}">
 </head>
 <body>
     <h3>Upload Status Summery</h3>
