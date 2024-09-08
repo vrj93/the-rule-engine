cce7f1cb2eb70e9c5ac680628146dfe11230ef3b
Vivek Joshi <vrj022@gmail.com>
2024-09-08 15:29:42 +0530

    FileScanStatusQueue initial delay, Error handling.

--- app/Jobs/FileScanStatusQueue.php
+++ app/Jobs/FileScanStatusQueue.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use App\Services\RuleEngineService;
-use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
@@ -21,6 +20,7 @@ class FileScanStatusQueue implements ShouldQueue
      */
     public function __construct(array $data)
     {
+        $this->delay(30);
         $this->fileScanReq = $data;
     }
 
@@ -33,7 +33,7 @@ class FileScanStatusQueue implements ShouldQueue
         if ($progress >= 0 && $progress < 100) $this->release(30);
     }
 
-    public function failed(Exception $ex): void
+    public function failed($ex): void
     {
         Log::error($ex->getMessage());
     }
